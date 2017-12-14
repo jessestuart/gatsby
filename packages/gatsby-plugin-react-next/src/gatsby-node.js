251e51281434efc9f30899fb4ed687857b2510cc
Jesse Stuart <jdstuart@icloud.com>
2017-12-13 19:37:44 -0500

    [deps] Update React to 16.2

--- packages/gatsby-plugin-react-next/src/gatsby-node.js
+++ packages/gatsby-plugin-react-next/src/gatsby-node.js
@@ -4,8 +4,8 @@ const React = require(path.resolve(process.cwd(), `node_modules/react`))
 exports.modifyWebpackConfig = ({ config, stage }) => {
   if (React.version.slice(0, 2) !== `16`) {
     config._config.resolve.alias = {
-      react: `gatsby-plugin-react-next/node_modules/react`,
-      "react-dom": `gatsby-plugin-react-next/node_modules/react-dom`,
+      react: `js-gatsby-plugin-react-next/node_modules/react`,
+      "react-dom": `js-gatsby-plugin-react-next/node_modules/react-dom`,
     }
   }
 
