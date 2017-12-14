9113c38d19e68ad0832a6259d761c94daa1027b0
Jesse Stuart <jdstuart@icloud.com>
2017-12-13 21:38:44 -0500

    [commit message skipped]

--- .babelrc.js
+++ .babelrc.js
@@ -4,45 +4,44 @@ function preset(context, options = {}) {
   const { browser = false, debug = false } = options
   const { NODE_ENV, BABEL_ENV } = process.env
 
-  const PRODUCTION = (BABEL_ENV || NODE_ENV) === "production"
+  const PRODUCTION = (BABEL_ENV || NODE_ENV) === `production`
 
   const browserConfig = {
-    useBuiltIns: false,
+    forceAllTransforms: true,
     targets: {
       browsers: PRODUCTION
-        ? [`last 4 versions`, `safari >= 7`, "ie >= 9"]
+        ? [`last 4 versions`, `safari >= 7`, `ie >= 9`]
         : [`last 2 versions`, `not ie <= 11`, `not android 4.4.3`],
-      uglify: PRODUCTION,
     },
+    useBuiltIns: false,
   }
 
   const nodeConfig = {
     targets: {
-      node: PRODUCTION ? 4.0 : "current",
+      node: PRODUCTION ? 4.0 : `current`,
     },
   }
 
   return {
     presets: [
       [
-        r("@babel/preset-env"),
+        r(`@babel/preset-env`),
         Object.assign(
           {
             loose: true,
             debug: !!debug,
             useBuiltIns: true,
-            modules: "commonjs",
+            modules: `commonjs`,
           },
           browser ? browserConfig : nodeConfig
         ),
       ],
-      r("@babel/preset-react"),
-      r("@babel/preset-flow"),
+      r(`@babel/preset-react`),
+      r(`@babel/preset-flow`),
     ],
     plugins: [
-      r("@babel/plugin-transform-object-rest-spread"),
       [
-        r("@babel/plugin-transform-runtime"),
+        r(`@babel/plugin-transform-runtime`),
         {
           // we are only polyfilling the node environment
           // so we need to enable the runtime replacements for the browser preset
